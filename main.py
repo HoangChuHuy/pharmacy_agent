ba1671f776dc89d7d845301a2556233a796f7946
Chu Huy Hoang <chuhuyhoang56@gmail.com>
2025-06-17 12:44:14 +0700

    add gradio interface

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
 from src.agent.agent import RAGAgentSystem
 from loguru import logger
+import gradio as gr
 
 
+chat_history = []
+rag_agent = RAGAgentSystem()
+
+def chatbot_interface(user_input):
+    global chat_history
+    # Gọi RAG flow của bạn ở đây
+    response = rag_agent.run(user_input)
+    chat_history.append((user_input, response))
+    return chat_history
+
+# Hàm reset lịch sử
+def reset_chat():
+    global chat_history
+    chat_history = []
+    rag_agent.memory.clear()
+    return "", gr.update(value="")
 
 if __name__== '__main__':
-    rag_agent = RAGAgentSystem()
-    while True:
-        question1 = input("Nhập câu hỏi mà bạn muốn hỏi: ")
-        if question1:
-            logger.info("User query: {}", question1)
-            answer1 = rag_agent.run(question1)
-            logger.info("\n✅ [answer]:\n {}", answer1)
-        else:
-            break
+    with gr.Blocks() as demo:
+        gr.Markdown("## 🤖 Chatbot with RAG + Reset")
+        chatbot = gr.Chatbot()
+        with gr.Row():
+            txt = gr.Textbox(placeholder="Nhập câu hỏi...", show_label=False)
+            reset_btn = gr.Button("🔄 Reset")
+        
+        # Sự kiện gửi tin nhắn
+        txt.submit(fn=chatbot_interface, inputs=txt, outputs=chatbot)
+        
+        # Sự kiện nút reset
+        reset_btn.click(fn=reset_chat, outputs=[txt, chatbot])
+
+    demo.launch(share=True, server_port=8080)
+
+
